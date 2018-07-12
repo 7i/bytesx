62b2f93919f9d15c0c3ffa2eddb8b6b9d8dc309d
Daniel Liden <github.7i@gmail.com>
2018-07-12 13:28:42 +0200

    Update hammingDistance_amd64.s
    Minor update to comments

--- hammingDistance_amd64.s
+++ hammingDistance_amd64.s
@@ -33,14 +33,14 @@ TEXT ·HammingDistance(SB),7,$0
 //; we have all CPU fetures that we need before calling the new function.
 
 //; Check that we have access to all instructions we need
-	MOVQ $1, AX	//; AX=1: Processor Info and Feature Bits
-	CPUID				//; MOVOU (MOVDQU) needs sse2, bit 26 in EDX
-						//; PCMPEQB needs sse2, bit 26 in EDX
-						//; PMOVMSKB needs sse2, bit 26 in EDX
-						//; sse2 came with the Pentium 4 in 2000
-						//; POPCNTQ needs popcnt bit 23 ECX
-						//; The popcnt CPUID feture is the newest feture that we need.
-						//; popcnt came with the Nehalem architecture in 2008.
+	MOVQ $1, AX		//; AX=1: Processor Info and Feature Bits
+	CPUID			//; MOVOU (MOVDQU) needs sse2, bit 26 in EDX
+				//; PCMPEQB needs sse2, bit 26 in EDX
+				//; PMOVMSKB needs sse2, bit 26 in EDX
+				//; sse2 came with the Pentium 4 in 2000
+				//; POPCNTQ needs popcnt bit 23 ECX
+				//; The popcnt CPUID feture is the newest feture that we need.
+				//; popcnt came with the Nehalem architecture in 2008.
 
 	ANDQ $0x4000000, DX	//; AND against bit 26 in EDX
 	JZ missingSSE2
@@ -52,7 +52,7 @@ TEXT ·HammingDistance(SB),7,$0
 
 hdHuge:
 //; Copy in 64 bytes of a and b in to the xmm registers X0 to X7
-	MOVOU (SI), X0			//;						MOVDQU on Sandy Bridge (m128, xmm) 1 cycles/instructions and 3 in latency
+	MOVOU (SI), X0	//;						MOVDQU on Sandy Bridge (m128, xmm) 1 cycles/instructions and 3 in latency
 	MOVOU 16(SI), X2
 	MOVOU 32(SI), X4
 	MOVOU 48(SI), X6
@@ -63,7 +63,7 @@ hdHuge:
 
 //; PCMPEQB will set X0, X2, X4 and X6 to only contain 0xFF if corresponding
 //; bytes in a and b are the same.
-	PCMPEQB X1, X0			//;						Sandy Bridge (xmm xmm) 0.5 cycles/instructions and 1 in latency
+	PCMPEQB X1, X0	//;						Sandy Bridge (xmm xmm) 0.5 cycles/instructions and 1 in latency
 	PCMPEQB X3, X2
 	PCMPEQB X5, X4
 	PCMPEQB X7, X6
@@ -71,13 +71,13 @@ hdHuge:
 //; PMOVMSKB takes the highest bit from every byte in X0, X2, X4 and X6
 //; respectivly and copys them in to the 16 least significant bits of
 //; R9, BX, CX and DX respectivly.
-	PMOVMSKB X0, R9		//;						Sandy Bridge (reg im) 1 cycles/instructions and 2 in latency
+	PMOVMSKB X0, R9	//;						Sandy Bridge (reg im) 1 cycles/instructions and 2 in latency
 	PMOVMSKB X2, BX
 	PMOVMSKB X4, CX
 	PMOVMSKB X6, DX
 
 //; Move all 64 resulting bits in to R9
-	SHLQ $48, R9		//;	XX______				Sandy Bridge (reg im) 0.5 cycles/instructions and 1 in latency
+	SHLQ $48, R9	//;	XX______				Sandy Bridge (reg im) 0.5 cycles/instructions and 1 in latency
 	SHRQ $16, R9:BX	//;	XXXX____				Sandy Bridge (reg im im) 0.5 cycles/instructions
 	SHRQ $16, R9:CX	//;	XXXXXX__
 	SHRQ $16, R9:DX	//;	XXXXXXXX
@@ -89,12 +89,12 @@ hdHuge:
 //; Clear BX so we can add the POPCNTQ result directly to AX (also break
 //; dependency chain as POPCNTQ has a false dependency on dest)
 	XORQ BX, BX
-//; Count the number of set bits and save to R9
+//; Count the number of set bits and save to AX
 	POPCNTQ	R9, BX
 	ADDQ BX, AX
 
 //; Update the pointers and counter then jump back to the start of the loop.
-	ADDQ $64, SI 			//;						Sandy Bridge (reg im) 0.33 cycles/instructions and 1 in latency
+	ADDQ $64, SI 	//;						Sandy Bridge (reg im) 0.33 cycles/instructions and 1 in latency
 	ADDQ $64, DI
 	SUBQ $64, R8
 
